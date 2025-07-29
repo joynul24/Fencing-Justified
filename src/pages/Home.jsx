44f79c58034e02983cd1f731336625147b680453
joynul24 <joynula919@gmail.com>
2025-07-30 00:33:09 +0600

    I have added the Landscape product section in the home.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import Agricultural from "./Agricultural/Agricultural";
+import Landscape from "./Landscape/Landscape";
 import Slider from "./Slider/Slider";
 
 const Home = () => {
@@ -6,6 +7,7 @@ const Home = () => {
         <div>
             <Slider></Slider>
             <Agricultural></Agricultural>
+            <Landscape></Landscape>
         </div>
     );
 };
